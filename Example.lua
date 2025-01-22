a470ef1eb4c3479cf143b29e3331e4b1b7a5d379
FatherJustice <80711287+FatherJustice@users.noreply.github.com>
2025-01-22 16:51:52 +0000

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,17 +1,10 @@
--- New example script written by wally
--- You can suggest changes with a pull request or something
-
-local repo = 'https://raw.githubusercontent.com/FatherJustice/UI-Lib-RP/refs/heads/main/Example.lua?token=GHSAT0AAAAAAC5RCMDFIM4RDXRNJDTCZ3BGZ4RD3HA'
-
+local repo = 'https://raw.githubusercontent.com/FatherJustice/UI-Lib-RP/main/'
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
-
 local Window = Library:CreateWindow({
-    -- Set Center to true if you want the menu to appear in the center
-    -- Set AutoShow to true if you want the menu to appear when it is created
     -- Position and Size are also valid options here
-    -- but you do not need to define them unless you are changing them :)
+    -- but you do not need to define them unless you are changing them
 
     Title = 'Example menu',
     Center = true,
